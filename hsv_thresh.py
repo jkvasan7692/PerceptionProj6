1853aed43b5d2f1ccb273093afc3839dbd253b06
Suyash Yeotikar <suyashyeotikar@gmail.com>
2019-05-15 21:31:24 -0400

    SVM code

--- hsv_thresh.py
+++ hsv_thresh.py
@@ -8,6 +8,7 @@ Created on Wed May 15 14:31:19 2019
 import os,sys
 import numpy as np
 import math
+from sklearn import svm
 
 try:
     sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
@@ -53,16 +54,73 @@ def hsv_thresh(image):
 	ret,thresholded_image = cv.threshold(sat_channel,120,255,cv.THRESH_BINARY)
 	show(thresholded_image,"thresh",0)
 
+def read_training_images():
+    labels_list = [45,21,38,35,17,1,14]
+    training_directory = '../Training/'
+    training_data = []
+    for dirname,dirnames,filenames in os.walk(training_directory):
+        for dir_name in dirnames:
+            label_dir_str = dir_name[-5:]
+            if int(label_dir_str) in labels_list:
+                for filename in os.listdir(training_directory+dir_name):
+                    if ".ppm" in filename:
+                        image_path = training_directory+dir_name+'/'+filename
+                        image = cv.imread(image_path,0)
+                        label_image_tup = (image,int(label_dir_str))
+                        training_data.append(label_image_tup)
+    return training_data
+
+
+def get_hog_features_labels(training_data):
+    win_size=(64,64)
+    block_size= (32,32)
+    cell_size= (8,8)
+    block_stride = (8,8)
+    nbins = 18
+    hog = cv.HOGDescriptor(_winSize=win_size,
+                            _blockSize=block_size,
+                            _blockStride=block_stride,
+                            _cellSize=cell_size,
+                            _nbins=nbins,
+                            _signedGradient=True)
+    labels_list = []
+    hog_features_list = []
+    for data in training_data:
+        image = data[0]
+        label = data[1]
+        resized_image = cv.resize(image,(64,64))
+        hog_features = hog.compute(resized_image)
+        print_debug(hog_features.shape)
+        hog_features_list.append(hog_features)
+        labels_list.append(label)
+    hog_array = np.array(hog_features_list)
+    return hog_array,labels_list
+
+
+def train_svm(hog_features,labels):
+    label_array = np.array(labels)
+    hog_features = hog_features.reshape((hog_features.shape[0],hog_features.shape[1]))
+    clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
+    clf.fit(hog_features,label_array)
+    print_debug(clf)
+
+
+
+
+
 
 
 files_list = os.listdir(images_dir)
 files_list.sort()
-for file_name in files_list:
-    if ".jpg" in file_name:
-        image = cv.imread(images_dir+file_name)
-        #final = np.hstack((image, white_balance_loops(image)))
-        thresh_image = hsv_thresh(image)
+train_data = read_training_images()
+hog_features,labels = get_hog_features_labels(train_data)
+svm_trained = train_svm(hog_features,labels)
 
-        show(image,"image",0)
+# for file_name in files_list:
+#     if ".jpg" in file_name:
+#         image = cv.imread(images_dir+file_name)
+#         #final = np.hstack((image, white_balance_loops(image)))
+#         thresh_image = hsv_thresh(image)
+#         show(image,"image",0)
 
 
